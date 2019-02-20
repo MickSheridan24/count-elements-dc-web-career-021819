21127f76eefadbe387a811f63f2109a9e3f7870d
Mickey <micksheridan.24@gmail.com>
2019-02-20 10:47:41 -0500

    submit

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,14 @@
 def count_elements(array)
   # code goes here
+  ret = {}
+
+  array.each do |e|
+    if (!ret.has_key?(e))
+      ret[e] = 1
+    else
+      ret[e] += 1
+    end
+  end
+  ret
 end
  
